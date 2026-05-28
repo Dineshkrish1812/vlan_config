1df34bf34d4979a79541fe06053b5711f4e126d0
Dineshkrish1812 <dineshkrish1812@gmail.com>
2026-05-28 08:01:45 +0000

    updated

--- run-playbook.sh
+++ run-playbook.sh
@@ -5,4 +5,11 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 export ANSIBLE_CONFIG="$PWD/ansible.cfg"
 
-exec ansible-playbook -i inventory.ini playbook.yml "$@"
+playbook="${1:-playbooks/vlan.yml}"
+if [[ "$playbook" == -* ]]; then
+  playbook="playbooks/vlan.yml"
+else
+  shift || true
+fi
+
+exec ansible-playbook -i inventory.ini "$playbook" "$@"
